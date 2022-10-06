5cc274d676139fd977074349683a0f7a22d8ac17
dev-th-kang <kang.1107.th@gmail.com>
2022-10-06 19:29:39 +0900

    feature : todo 기능 client에서 확인완료 cor 추가

--- config/db.py
+++ config/db.py
@@ -1,5 +1,47 @@
+from unittest import result
 from pymongo import MongoClient
+from datetime import datetime
 mongo_url = "mongodb://root:Kk2001devth@127.0.0.1:27017/"
 conn = MongoClient(mongo_url)
+#db = todo_query
+todo_query = conn.fastapi.todo
+'''
+#사용자 관련
+def createUser(user):
+    
+def deleteUser(user):
 
+def updateUser(user):
+
+def lookupUser(user):
+
+def lookupUsers():
+'''
+
+# toDo list
+def createTodo(todo):
+    res = dict(todo)
+    res["date"] = datetime.today()
+    res =todo_query.insert_one(res)
+    return res
+
+def deleteTodo(title):
+    res = todo_query.delete_one(title)
+    return res
+
+def updateTodo(filter,set):
+    res= todo_query.update_one(filter,set)
+    return res
+
+def lookupTodo(title):
+    results = todo_query.find_one(title)
+    res = {"title":results['title'],"contents":results['contents'],"date":results['date']}
+    return res
+
+def lookupTodos():
+    results = todo_query.find()
+    res = []
+    for s in results:
+        res.append({"title":s['title'],"contents":s['contents'],"date":s['date']})
+    return res
 #"mongodb://localhost:27017/test"
--- routes/route.py
+++ routes/route.py
@@ -2,54 +2,46 @@ from distutils.command.config import config
 from typing import Collection
 from unittest import result
 from fastapi import APIRouter
-from config.db import conn
+from config import db
 from models.todo import Todo
-from datetime import datetime
 router = APIRouter()
-db = conn.fastapi.todo
 
 ## db 관려 함수 사용 따로 분리 필요
 ##  auto increase num 으로 구분 짓고싶음
 ## 수정하면 수정한 날짜로 업데이트 되게 변경
 ## title은 겹칠수도 있는데 no가 안겹치게 
 ## 현재는 title 중점으로 만들었음
+
+
+
+#login/join/logout
+
+
+#todo
 @router.post("/todo")
 async def createTodo(todo:Todo):
-    print(todo)
-    today = datetime.today()
-    resValue = dict(todo) 
-    resValue["date"] =today
-    print(resValue)
-    results =db.insert_one(resValue)
-    return "insert";
-
+    x = db.createTodo(dict(todo))
+    #x.inserted_id
+    return {'msg':'succeed'};
 
 @router.get("/todo/{title}")
 async def readTodo(title:str):
-    result = db.find_one(dict({"title":title}))
-    resValue = {"title":result['title'],"contents":result['contents'],"date":result['date']}
-    return resValue
+    res = db.lookupTodo(dict({"title":title}))
+    return res
 
     
 @router.get("/todo")
 async def readAllTodo():
-    results = db.find()
-    resValue = []
-    for s in results:
-        print(s)
-        resValue.append({"title":s['title'],"contents":s['contents'],"date":s['date']})
-    return resValue
+    res_array= db.lookupTodos()
+    return res_array
 
 @router.put("/todo/{title}")
 async def updateTodo(title:str,todo:Todo):
-    print(todo)
-    result = db.update_one({"title":title},{"$set":dict(todo)})
-    print(result)
-    return {"msg":"good"}
+    x = db.updateTodo({"title":title},{"$set":dict(todo)})
+    return {"msg":"succeed update"}
 
 
 @router.delete("/todo/{title}")
 async def deleteTodo(title:str):
-    result = db.delete_one({"title":title})
-    print(result)
-    return {"msg":"good"}
+    x = db.deleteTodo({"title":title})
+    return {"msg":"succeed del"}
